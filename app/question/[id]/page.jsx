849aa9c4f28d889fde872bf3b41c169577540516
detectivemerry <jiiasheng@hotmail.com>
2024-04-02 17:38:59 +0800

    fix styling

--- app/question/[id]/page.jsx
+++ app/question/[id]/page.jsx
@@ -1,45 +1,7 @@
 import { getDataFromXlsx } from "@/app/lib/data";
 import Answer from "./Answer";
 
-//async function getQuestions(){
-//  const questions = await fetch("http://localhost:3000/api/questions", {
-//    method : "GET",
-//    next : {revalidate : 0}
-//  })
-//  .then((res) => {
-//    if(!res.ok)
-//      throw Error("Error connecting to server.")
-//    return res.json();
-//  })
-//  .then((data) => {
-//    return data
-//  })
-//  .catch((error) => {
-//    console.error(error);
-//  })
-//
-//  if(questions)
-//    return { questions : questions.questions, error : ""};
-//  else 
-//    return { questions : "", error : "There was an problem with retrieving the questions"};
-//}
-
-//export async function generateStaticParams(){
-//  const questions = await getQuestions();
-//  const params = questions.map((q) => ({
-//    id : String(q.question_id)
-//  }))
-//
-//  return params
-//}
-
-//export const dynamicParams = true;
-
 export default async function page({params}) {
-  try {
-    //const {questions, error} = await getQuestions();
-    //if(error)
-      //throw new Error(error)
 
     const questions = await getDataFromXlsx();
     const maxNum = questions.length;
@@ -47,7 +9,7 @@ export default async function page({params}) {
 
     return(
     <main className = "flex flex-col text-center items-center h-screen">
-      <div className = "flex justify-center m-3 my-12">
+      <div className = "flex justify-center m-3 my-6">
         {params.id} / {maxNum}
       </div>
       <div className = "border-2 border-black h-2/5 w-full lg:w-2/5">
@@ -62,7 +24,5 @@ export default async function page({params}) {
     </main>
     )
 
-  } catch(error){
-    throw new Error(error);
   }
-}
+
--- app/result/page.jsx
+++ app/result/page.jsx
@@ -5,7 +5,6 @@ import { useContext } from 'react'
 import { AppContext } from '@/app/context/App.context';
 
 export default function Result() {
-  try {
 
   const context = useContext(AppContext)
   const [ image, setImage ] = useState('') 
@@ -19,26 +18,18 @@ export default function Result() {
 
   useEffect(() => {
     const type = mode(context.answers)
-    console.log(type + '.png')
     setImage(type + '.png')
   }, [context.answers])
-    //assign result based on context
-  
+
     return (
     <div className = "bg-gray-700 lg:flex lg:flex-col lg:items-center">
       {image &&
         <Image
           src = {require(`@/input/images/${image}`)}
           alt={`Image of result`}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "auto", height: "80%" }}
         />
       }
     </div>
   )
-  } catch(error){
-    throw new Error("internal server error")
-  }
+   
 }
